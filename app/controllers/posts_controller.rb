ba48bf7ab7f5d537eef9cd4a417f04c9760e44ad
drumnation <davidmieloch@gmail.com>
2017-04-10 17:24:08 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,15 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
+  def new
+  end
+
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+    # create a new Post object with the values from params
+    # and then redirect to the index page
+  end
+
 end
